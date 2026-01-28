6fca897d17cc84b88a9613bf1edd504b03a77145
OmkarRankhamb <omrankhamb7@gmail.com>
2026-01-28 22:34:23 +0530

    Object_Properties

---
+++ Object_Properties/First.js
@@ -0,0 +1,13 @@
+
+let obj = {
+    "email":"om",
+    "mobile":"234567890",
+    "city":"pune"
+};
+
+console.log(obj);
+console.log(Object.values(obj));
+console.log(Object.keys(obj));
+console.log(Object.entries(obj));
+
+
