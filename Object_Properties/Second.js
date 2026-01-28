2d86f1bc106ac83895524a8af08386721a7b3a12
OmkarRankhamb <omrankhamb7@gmail.com>
2026-01-28 23:01:46 +0530

    Object P

---
+++ Object_Properties/Second.js
@@ -0,0 +1,25 @@
+
+let Student={
+    "name": "Alan Jones",
+    "email ": "Aloan",
+    "City": "pune"
+}
+
+console.log(Student);
+console.log(
+
+Object.hasOwn(Student,"Email"));
+
+console.log("********************************************");
+
+console.log(Object.is(4,15));
+
+console.log();
+
+console.log(Object.fromEntries([['a'],['b']]));
+console.log(Object.fromEntries([['a','1'],['b',2]]));
+
+
+
+
+
