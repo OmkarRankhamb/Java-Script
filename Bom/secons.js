e1fd7a924a197a04eba5ff1249a9197199a77e78
OmkarRankhamb <omrankhamb7@gmail.com>
2026-02-24 15:25:51 +0530

    Bom

---
+++ Bom/secons.js
@@ -0,0 +1,20 @@
+
+function alertwidth(){
+    console.log("Screen..");
+    console.log(screen.availHeight);
+    console.log(screen.availWidth);
+    console.log(screen.colorDepth);
+    console.log(screen.pixelDepth);
+    
+    
+    
+    
+    
+}
+
+function goto  (){
+
+    window.location.href="https://www.google.com";
+
+
+}
